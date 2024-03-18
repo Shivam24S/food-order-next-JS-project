2eacb6b22afb03f7809d356e5de3c15337e8ac24
Shivam24S <shivamshrivastav8055@gmail.com>
2024-03-18 19:36:22 +0530

    handling not found page

---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFoundPage() {
+  return (
+    <main className="not-found">
+      <h1>page not Found</h1>
+      <p>Requested page or resource Not Found</p>
+    </main>
+  );
+}
