42096c5577a340b3a41ca0dd5ae388dcec9e67f5
Shivam24S <shivamshrivastav8055@gmail.com>
2024-03-18 18:42:20 +0530

    fetching data

--- app/meals/page.js
+++ app/meals/page.js
@@ -2,8 +2,10 @@ import MealsGrid from "@/component/meals/meals-grid";
 import classes from "./page.module.css";
 
 import Link from "next/link";
+import { getMeals } from "@/lib/meals";
 
-export default function Meals() {
+export default async function Meals() {
+  const meals = await getMeals();
   return (
     <>
       <header className={classes.header}>
@@ -19,7 +21,7 @@ export default function Meals() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={[]} />
+        <MealsGrid meals={meals} />
       </main>
     </>
   );
--- component/meals/meals-grid.js
+++ component/meals/meals-grid.js
@@ -1,9 +1,10 @@
 import MealItem from "./meals-item";
+import classes from "./meals-grid.module.css";
 
 export default function MealsGrid({ meals }) {
   return (
     <>
-      <ul>
+      <ul className={classes.meals}>
         {meals.map((meal) => (
           <li key={meal.id}>
             <MealItem {...meal} />
