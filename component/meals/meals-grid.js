40d56673c57af00450302ac423d45d97d166da19
Shivam24S <shivamshrivastav8055@gmail.com>
2024-03-18 16:20:03 +0530

    meals component related data added

---
+++ component/meals/meals-grid.js
@@ -0,0 +1,15 @@
+import MealItem from "./meals-item";
+
+export default function MealsGrid({ meals }) {
+  return (
+    <>
+      <ul>
+        {meals.map((meal) => (
+          <li key={meal.id}>
+            <MealItem {...meal} />
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
