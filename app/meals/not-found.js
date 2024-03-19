2c35c0235b882fb6177040fd52228fa4b82b4a2b
Shivam24S <shivamshrivastav8055@gmail.com>
2024-03-19 10:12:24 +0530

    handling component specific not found page

---
+++ app/meals/not-found.js
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <>
+      <div className="error">
+        <h1>Meal not Found</h1>
+        <p>failed to fetch requested meal </p>
+      </div>
+    </>
+  );
+}
